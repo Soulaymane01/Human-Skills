bf0f4e2945e72d8ec7a3cff1f6250cb513afd4de
Soulaymane01 <soulaynadi@gmail.com>
2025-02-16 22:08:33 +0100

    Allowd requests from frontend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
